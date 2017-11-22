08fcc8ea46b4d53a7c81d000aa1b6bfb8e872d67
Anthony <anthony_bouillon@outlook.fr>
2017-11-22 12:25:28 +0100

    Insertion de données

---
+++ bdd.sql
@@ -0,0 +1,22 @@
+Exercice 1
+Dans la table languages insérez les données suivantes :
+
+    JavaScript version 5 :
+    PHP version 5.2 :
+    PHP version 5.4 :
+    HTML version 5.1 :
+    JavaScript version 6 :
+    JavaScript version 7 :
+    JavaScript version 8 :
+    PHP version 7 :
+Ligne de commande :  INSERT INTO languages VALUES (1,'JavaScript', 5), (2,'PHP', 5.2), (3,'PHP', 5.4), (4,'HTML', 5.1), (5,'JavaScript', 6), (6,'JavaScript', 7), (7,'JavaScript', 8), (8,'PHP', 7);
+
+Exercice 2
+Dans la table frameworks insérez les données suivantes :
+
+    Symfony version 2.8 :
+    Symfony version 3 :
+    Jquery version 1.6 :
+    Jquery version 2.10 :
+
+Ligne de commande : INSERT INTO frameworks VALUES (1,'Symfony', 2.8), (2,'Symfony', 3), (3,'PHP', 1.6), (4,'HTML', 2.10);
